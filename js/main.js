5627be914dc716b236d2d2df166b3bca963fa72f
CamiloTrabajo26 <camiloarintrabajo@gmail.com>
2026-01-06 10:06:40 -0300

    Cambio de header 2.0

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log("Soporte Gamer cargado correctamente");
+
+// Ejemplo: alerta al hacer clic (opcional)
+const btn = document.getElementById("btn-whatsapp");
+
+if (btn) {
+  btn.addEventListener("click", () => {
+    console.log("Click en WhatsApp");
+  });
+}
